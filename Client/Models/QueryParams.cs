e7cdfca3d2c08ca0bb9b26665eca4ab16656dfb4
mvinicius2k <marcos.vinicius124816@gmail.com>
2024-03-04 18:02:32 -0300

    Blazor layout

---
+++ Client/Models/QueryParams.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Client.Models;
+
+public interface IQueryParams
+{
+    public int Page { get; }
+    public int PageSize { get; }
+    public string? OrderBy { get; }
+    public (string fieldName, string value)? StringSearch { get; }
+}
